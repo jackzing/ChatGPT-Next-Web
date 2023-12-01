d0b505a7639e47ba92026a64dbb7a7de302945d3
jackzing <jackiszing@gmail.com>
2023-12-01 15:47:08 +0800

    add chatgpt prefix

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -9,8 +9,8 @@ const en: LocaleType = {
   WIP: "Coming Soon...",
   Error: {
     Unauthorized: isApp
-      ? "Invalid API Key, please check it in [Settings](/#/settings) page."
-      : "Unauthorized access, please enter access code in [auth](/#/auth) page, or enter your OpenAI API Key.",
+      ? "Invalid API Key, please check it in [Settings](/chatgpt#/settings) page."
+      : "Unauthorized access, please enter access code in [auth](/chatgpt#/auth) page, or enter your OpenAI API Key.",
   },
   Auth: {
     Title: "Need Access Code",
@@ -443,8 +443,8 @@ const en: LocaleType = {
   },
   Exporter: {
     Description: {
-      Title: "Only messages after clearing the context will be displayed"
-    },  
+      Title: "Only messages after clearing the context will be displayed",
+    },
     Model: "Model",
     Messages: "Messages",
     Topic: "Topic",
